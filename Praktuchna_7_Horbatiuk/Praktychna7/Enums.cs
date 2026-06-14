c6c80bd3a2add5fe077b7315ecdbc60cc7cf0dda
etvergonni <aleksandragorbatuk7@gmail.com>
2026-06-14 16:55:46 +0300

    chore: базова структура ПР7 на основі ПР1-6 (успадковані класи)

---
+++ Praktuchna_7_Horbatiuk/Praktychna7/Enums.cs
@@ -0,0 +1,9 @@
+namespace Praktychna7;
+
+public enum StudentStatus
+{
+    Active,
+    AcademicLeave,
+    Expelled,
+    Graduated
+}
---
+++ Praktuchna_7_Horbatiuk/Praktychna7/Triangle.cs
@@ -0,0 +1,40 @@
+using System;
+
+namespace Praktychna7;
+
+public class Triangle : Shape
+{
+    public double SideA { get; set; }
+    public double SideB { get; set; }
+    public double SideC { get; set; }
+
+    public Triangle(string color, double a, double b, double c) : base("Трикутник", color)
+    {
+        SideA = a;
+        SideB = b;
+        SideC = c;
+    }
+
+    public override double CalculatePerimeter() => SideA + SideB + SideC;
+
+    public override double CalculateArea()
+    {
+        double s = CalculatePerimeter() / 2;
+        double underRoot = s * (s - SideA) * (s - SideB) * (s - SideC);
+        return underRoot > 0 ? Math.Sqrt(underRoot) : 0;
+    }
+
+    public override string GetDescription() => $"Трикутник зі сторонами {SideA:F2}, {SideB:F2}, {SideC:F2}";
+
+    public override void Resize(double factor)
+    {
+        SideA *= factor;
+        SideB *= factor;
+        SideC *= factor;
+    }
+
+    public override void Draw()
+    {
+        Console.WriteLine($" /\\   Трикутник ({Color}), сторони {SideA:F2}, {SideB:F2}, {SideC:F2}");
+    }
+}
